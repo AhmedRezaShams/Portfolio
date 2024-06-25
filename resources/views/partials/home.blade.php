4f1dfc5c6a061cefc1beb43f4d05e69570d3d3eb
Ahmed Reza <ahmedrezashams4@gmail.com>
2024-06-25 16:37:00 +0600

    home edited

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -1,18 +1,18 @@
 <!-- Hero Section -->
 <section id="hero" class="hero section">
 
-<img src="assets/img/hero-bg.jpg" alt="">
+<img src="assets\img\rezalink (1).png" alt="">
 
 <div class="container" data-aos="zoom-out">
   <div class="row justify-content-center">
     <div class="col-lg-9">
-      <h2>Brandon Johnson</h2>
-      <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer">Designer</span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+      <h2>Ahmed Reza Riday</h2>
+      <p>I'm a <span class="typed" data-typed-items="Software Engineer, Developer, Robotics enthused, Junior Robotics Engineer at Texon">Designer</span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
       <div class="social-links">
-        <a href="#"><i class="bi bi-twitter-x"></i></a>
-        <a href="#"><i class="bi bi-facebook"></i></a>
-        <a href="#"><i class="bi bi-instagram"></i></a>
-        <a href="#"><i class="bi bi-linkedin"></i></a>
+        <!-- <a href="#"><i class="bi bi-twitter-x"></i></a> -->
+        <a href="https://www.facebook.com/ahmedbin.shams.98"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/ahmed_bin_shams/"><i class="bi bi-instagram"></i></a>
+        <a href="https://bd.linkedin.com/in/ahmed-reza-shams-3a65212a9"><i class="bi bi-linkedin"></i></a>
       </div>
     </div>
   </div>
