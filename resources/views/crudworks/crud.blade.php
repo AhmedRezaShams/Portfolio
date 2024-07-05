5e32c20d66134a04c670997165db14d17df1377c
Ahmed Reza <ahmedrezashams4@gmail.com>
2024-07-05 22:37:23 +0600

    üpdate not working

--- resources/views/crudworks/crud.blade.php
+++ resources/views/crudworks/crud.blade.php
@@ -85,6 +85,7 @@ $(document).ready(function(){
 						<td>{{$item->Email}}</td>
 						<td>{{$item->Address}}</td>
 						<td>{{$item->Phone}}</td>
+						<td>{{$item->id}}</td>
 						<td>
 							<a href="#editEmployeeModal"  class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
 							<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
